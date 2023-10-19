93c9dca2156c345860bb5686d02b39118794c8ed
modernham <billy.dean.penley@gmail.com>
2023-10-18 21:08:21 -0500

    Create StartAX25.sh

---
+++ StartAX25.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+screen -S interface -dm bash -c "sudo socat -d -d -ly PTY,link=/dev/ttyq3 PTY,link=/dev/ptyq3"
+sleep 2
+sudo chmod 777 /dev/ttyq3
+sudo chmod 777 /dev/ptyq3
+sudo kissattach -l /dev/ptyq3 ax0
+sudo kissparms -c 1 -p ax0
+screen -S direwolf -dm bash -c "direwolf -d kn -c ~/Documents/direwolf.conf; exec sh"
